44819ba59f0f0d1cd570a7c07135335123871739
RakeshDP <rakeshpawar1895.rp@gmail.com>
2023-09-12 22:58:12 +0530

    submitting code-Tue Sep 12 2023 22:58:11 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,25 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react"
+import "../styles/App.css"
+import Cart from "./Cart"
+
+
+const App =()=>{
+
+    return(
+        <div>
+            <h1>Our Menu</h1>
+            <div className="btn">
+            <button>All</button>
+            <button>Breakfast</button>
+            <button>Lunch</button>
+            <button>Shakes</button> 
+            </div>
+            <Cart/>
+        </div>
+    )
+}
+
+export default App;
+
+
 
